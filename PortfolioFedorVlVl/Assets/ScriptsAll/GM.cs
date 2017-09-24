5adcc6d39088993655fc7b00bbc649ef51eabcba
Fedor <fedorvlvl@gmail.com>
2017-09-24 10:18:00 +0300

    addition to GM

--- PortfolioFedorVlVl/Assets/ScriptsAll/GM.cs
+++ PortfolioFedorVlVl/Assets/ScriptsAll/GM.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 #region My Unity portfolio 
 /*My portfolio
  * This project is Portfolio Fedor Vlasov
- * 1. Title
+ * 1. Title (select Ru or En, music and sound ON/OFF)
  * --------------------------------------------
  * 2. Menu (models, animations, VFX Unity, codes, resume, contacts )
  *           ....................
@@ -12,8 +12,9 @@ using UnityEngine;
  *    2) models (start camera fly accros area with my models
  *               if we want we can drow camera and come to models
  *               what we like to see better)
- *    3) animation (select 2d or 3d) after that we have opporunity to
- *               select models and use from list of animations it. *               
+ *    3) animation (select 2d, GUI animation, or 3d) after that we 
+ *                  have opporunity to select models and use from
+ *                  list of animations it.               
  *    4) vfx - effects particles and other
  *    5) codes change items for Hillary
  *    6) resume
