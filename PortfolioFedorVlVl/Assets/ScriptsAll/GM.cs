72b742fd1c1dc83da7788b0cbc2b44964d01d114
Fedor <fedorvlvl@gmail.com>
2017-09-24 09:46:19 +0300

    made same idea for UnityPortfolio in head GM script

---
+++ PortfolioFedorVlVl/Assets/ScriptsAll/GM.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#region My Unity portfolio 
+/*My portfolio
+ * This project is Portfolio Fedor Vlasov
+ * 1. Title
+ * --------------------------------------------
+ * 2. Menu (models, animations, VFX Unity, codes, resume, contacts )
+ *           ....................
+ *    1) Title - animation (4-5 s)  
+ *    2) models (start camera fly accros area with my models
+ *               if we want we can drow camera and come to models
+ *               what we like to see better)
+ *    3) animation (select 2d or 3d) after that we have opporunity to
+ *               select models and use from list of animations it. *               
+ *    4) vfx - effects particles and other
+ *    5) codes change items for Hillary
+ *    6) resume
+ *    7) contacts  
+ * */
+#endregion
+public class GM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
