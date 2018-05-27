36f7f4605fbcfaaa1340bcf58afbbf3cf3a20f87
TrueLyaLyaka <TrueLyaLyaka@gmail.com>
2018-05-27 21:58:31 +0300

    connect animation for Title text-buttons

---
+++ PortfolioFedorVlVl/Assets/ScriptsAll/GMAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMAnimation : MonoBehaviour {	
+	// Use this for initialization
+	void Start () {		
+		StartCoroutine("StartPortf");
+	}
+	IEnumerator StartPortf ()
+	{
+		yield return new WaitForSeconds(3.0f);
+		GetComponent<Animator>().SetLayerWeight(2,1);
+	}
+}
